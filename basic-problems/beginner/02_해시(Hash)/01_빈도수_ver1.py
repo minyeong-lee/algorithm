e6be59bdfd7bbfcd37b0b353d305e95809f0eb7c
minyeong-lee <lmy1366@gmail.com>
2024-08-19 23:37:21 +0900

    feat : 01_빈도수 문제 보완

--- basic-problems/beginner/02_해시(Hash)/01_빈도수_ver1.py
+++ basic-problems/beginner/02_해시(Hash)/01_빈도수_ver1.py
@@ -1,13 +1,25 @@
 def solution(nums):
     answer = -1
-    cnt = [0] * 1001
+    cnt = [0] * 1001    # 인덱스 번호가 1000까지 있어야 하므로 1001개를 잡아야 함
+    
+    # Direct-address table 방식의 해싱_빈도 수 카운팅
+    # 빈도 수 카운팅
     for x in nums:
         cnt[x] += 1
-
-    for i in range(1000, 0, -1):
+     
+    # 빈도 수가 1인 숫자 중에서 최대값 찾기
+    # 아이디어 : 1000부터 돌기 (i가 key 값이기 때문)
+    for i in range(1000, 0, -1):    # 1000부터 0까지 1씩 작아지면서 순회
         if cnt[i] == 1:
-            return i
-       
+            return i    # i가 큰 값부터 돌기 때문에 가능함
+                        # 최초로 빈도수가 1인 숫자 만나면 그게 바로 빈도수 1인 제일 큰 값
+    
+    
+    # 이 방법은 1부터 1000까지 다 순회하므로 위의 방법 적용하기
+    # for i in range(1, 1001):    # 요구사항 준수하여 1부터 시작해도 가능
+    #     if cnt[i] == 1:
+    #         answer = max(answer, i)
+            
     return answer
                             
                 
