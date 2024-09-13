13578e57d1066b3f1d5ad17ab08dfb93d28b72fa
minyeong-lee <lmy1366@gmail.com>
2024-09-14 00:47:24 +0900

    solve: 피보나치 재귀, 반복문 이용 구현

---
+++ basic-problems/beginner/10_재귀함수와_DFS(깊이우선탐색)/02_피보나치_수열_반복문_이용.py
@@ -0,0 +1,13 @@
+# 반복문으로 풀이
+# n에 구해야 할 피보나치 수열의 항 번호가 주어지면
+# 피보나치 수열의 n번째 항을 구해 return
+def DFS(n):
+    if n <= 2:
+        return 1
+
+    arr = [1, 1]
+    for i in range(n-2):
+        arr.append(arr[-2] + arr[-1])    
+    return arr[n-1]
+
+print(DFS(10))
