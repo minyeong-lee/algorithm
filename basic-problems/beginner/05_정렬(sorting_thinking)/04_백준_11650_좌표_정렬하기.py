04de7e1f4851beab3d1de5fca1f8c8372b087561
minyeong-lee <lmy1366@gmail.com>
2024-09-17 23:19:02 +0900

    solve : 백준_좌표 정렬하기 (정답 코드 입력방식_map 함수로 정수 변환 후 [a,b] 형태로 append 가능함)

--- basic-problems/beginner/05_정렬(sorting_thinking)/04_백준_11650_좌표_정렬하기.py
+++ basic-problems/beginner/05_정렬(sorting_thinking)/04_백준_11650_좌표_정렬하기.py
@@ -18,3 +18,18 @@ for i in range(N):
     points.append(x_y)
 
 print(solution(N, points))
+
+
+# 정답 코드
+# def solution(nums):
+#     nums.sort(key = lambda v : (v[0], v[1]))
+#     for x in nums:
+#         print(x[0], x[1])
+
+
+# n = int(input())
+# arr = []
+# for i in range(n):
+#     a, b = map(int, input().split())  # 각 입력 값을 map을 통해 정수로 변환함
+#     arr.append([a, b])    #변환된 값을 [a, b] 형태로 리스트에 저장함
+# solution(arr)
